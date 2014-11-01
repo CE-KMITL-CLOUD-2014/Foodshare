fd9fc70a1afab0a4ddcacf37975fa44190dc9144
l2ragon <kit_tiwat@hotmail.com>
2014-11-01 23:46:27 +0700

    Back n Order and Reserve

--- your-project-name/app/controllers/OrderController.php
+++ your-project-name/app/controllers/OrderController.php
@@ -5,6 +5,52 @@ class OrderController extends BaseController {
 	public function getOrder(){
 		return View::make('Order.OrderForm');	
 	}
+	public function postOrder()	{
+		
+		$validator = Validator::make(Input::all(),   //check condition
+			array(
+				'Name' => 'required|max:50|email|unique:auth',
+				'Phonenumber' => 'required|max:10',
+				'buildingtype' => 'required',
+				'HouseNumber' => 'required',
+				'Road' => 'required',
+				'street' => 'required',
+				'city' => 'required'
+			)
+		);
+		
+		if($validator->fails()){   //if fail redirect to register page
+			return Redirect::route('Order-menu')
+				->withErrors($validator)
+				->withInput();
+		}else{
+			$name = Input::get('name');  // retrieve inputs
+			$phonenumber = Input::get('phoenumber');
+			$buildingtype = Input::get('buildingtype');
+			$housenumber = Input::get('houseNumber') ;
+			$road = Input::get('road');
+			$street = Input::get('street');
+			$city = Input::get('city');
+			//Activation code
+			$Order = Order::create(array(   //create account in database
+				'name' => $name,
+				'phonenumber' => $phonenumber,
+				'buildingtype' => $buildingtype,
+				'houseNumber' => $housenumber,
+				'road' => $road,
+				'street' => $street,
+				'city' => $city,
+			));
+			
+			if($Order){
+			
+				//Send email
+				return Redirect::route('home')
+					->with('global','Your account has been created');
+			}
+		}
+		
+	}
 	public function setOrder(){
 		return View::make('Order.setOrder');
 	}
--- your-project-name/app/views/Order/OrderForm.blade.php
+++ your-project-name/app/views/Order/OrderForm.blade.php
@@ -1,52 +1,59 @@
 @extends("layout.main")
 @section("content")
-    <form role="form" class="form-order">
+    <form role="form" class="form-order" action="{{ URL::route('Order-post') }}" method="post">
        <div class="container-fluid">
           <h2>Information to order</h2>
           <p></p>
             <div class="form-group">
-                  <label for="examplename">Name</label>
-                  <input type="email" class="form-control" id="Name" placeholder="Name">
-                </div>
-                <div class="form-group">
-                  <label for="examplePhonenumber">Phone Number</label>
-                  <input type="Phone number" class="form-control" id="Phone number" placeholder="Phone number">
-                </div>
-              <div class="checkbox">
-                  <label>
-                    <input type="checkbox"> Apartment
-                  </label>
-                </div>
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox"> Home
-                  </label>
-                </div>
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox"> Office
-                  </label>
-                </div>
-                <div class="form-group">
-                  <label for="House Number">No.</label>
-                  <input type="House Number" class="form-control" id="HoseNumber" placeholder="No.">
-                </div>
-
-                <div class="form-group">
-                  <label for="Road">Road</label>
-                  <input type="Road" class="form-control" id="Road" placeholder="Road">
-                </div>
-
-                <div class="form-group">
-                  <label for="street">street</label>
-                  <input type="street" class="form-control" id="street" placeholder="street">
-                </div>
-
-                <div class="form-group">
-                  <label for="city">city</label>
-                  <input type="city" class="form-control" id="city" placeholder="city">
-                </div>
-
-                <a href="{{URL::route('home')}}" type="Order3" class="btn btn-default">Submit</a>
+              <label for="examplename">Name</label>
+              <input type="name" class="form-control" id="name" placeholder="Name">
+              @if($errors->has('name'))
+              {{$errors->first('name')}}
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="examplePhonenumber">Phone Number</label>
+              <input type="phonenumber" class="form-control" id="phonenumber" placeholder="Phone number">
+              @if($errors->has('phonenumber'))
+              {{$errors->first('phonenumber')}}
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="buildingtype">building type</label>
+              <input type="buildingtype" class="form-control" id="buildingtype" placeholder="buildingtype">
+              @if($errors->has('buildingtype'))
+              {{$errors->first('buildingtype')}}
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="HouseNumber">No.</label>
+              <input type="houseNumber" class="form-control" id="hoseNumber" placeholder="No.">
+            @if($errors->has('hoseNumber'))
+              {{$errors->first('hoseNumber')}}
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="Road">Road</label>
+              <input type="road" class="form-control" id="road" placeholder="Road">
+            @if($errors->has('road'))
+              {{$errors->first('road')}}
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="street">street</label>
+              <input type="street" class="form-control" id="street" placeholder="street">
+            @if($errors->has('street'))
+              {{$errors->first('street')}}
+              @endif
+            </div>
+            <div class="form-group">
+              <label for="city">city</label>
+              <input type="city" class="form-control" id="city" placeholder="city">
+            @if($errors->has('city'))
+              {{$errors->first('city')}}
+              @endif
+            </div>
+              <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+              {{ Form::token() }}
         </form>
 @stop
