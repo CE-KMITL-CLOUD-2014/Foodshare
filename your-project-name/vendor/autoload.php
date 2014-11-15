17384818588d543f22d6f6a808c47351df771ec6
minute123456 <darkarea1999@gmail.com>
2014-11-15 21:02:07 +0700

    first update

--- your-project-name/vendor/autoload.php
+++ your-project-name/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitb940486be67c1568ff0ae5a146a6b2c7::getLoader();
+return ComposerAutoloaderInitfa45a7608c31150c52314ff3f4d541e9::getLoader();
