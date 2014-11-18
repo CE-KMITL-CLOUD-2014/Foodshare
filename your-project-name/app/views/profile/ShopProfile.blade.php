cb81d6f4b6c807218104f3894226c2dc8922f5c8
l2ragon <kit_tiwat@hotmail.com>
2014-11-19 02:27:11 +0700

    new

--- your-project-name/app/views/profile/ShopProfile.blade.php
+++ your-project-name/app/views/profile/ShopProfile.blade.php
@@ -1,34 +1,6 @@
 @extends("layout.mainshop")
 @section("content")
-  <!-- <div class="container-fluid">
-      <div class="row">
-        <div class="col-xs-4 col-md-4">
 
-      
-         <p>{{ Session::get('nameshop')}}</p>
-		<br>
-		<p>Available:{{ Session::get('seat')}} Seat</p>
-        </div>
-        <div class="col-xs-4 col-md-4">
-          <button type="AllMenu" class="btn btn-primary">ShowAllMenu</button>
-			<br><br><br>
-		@foreach ($menus as $menu)
-			
-			<img width="75" height="75" alt="star" src="data:image/jpg;base64,{{{$menu->Image}}}" />
-			<br>
-			{{$menu->Namemenu }}  {{$menu->Price}} 
-			<br>
-			
-		@endforeach
-		
-		</div>
-		<div class="col-xs-4 col-md-4">
-			@foreach ($comments as $comment)
-					<a href="{{ URL::route('profile-user',$comment->Email)}}">{{ $comment->Email }} </a>:{{ $comment->Comment }} </br>
-			@endforeach
-		<div>
-      </div>
-    </div>-->
 
     <div class="col-md-12">
     <div style="margin-left:10px;">
